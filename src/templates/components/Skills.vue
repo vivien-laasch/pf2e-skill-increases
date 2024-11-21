65c4a1ea7f125bcadceb03ac41aa7d08552673c1
cuyima <vivilotta98@live.de>
2024-11-21 23:49:37 +0100

    compute bonuses

--- src/templates/components/Skills.vue
+++ src/templates/components/Skills.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
 import { computed } from "vue";
-import ProficiencyIndicator from "./Proficiency.vue";
+import Proficiency from "./Proficiency.vue";
 import { useSkillManagerStore } from "../../module/stores/SkillManagerStore";
+import { computeAttributeProgression } from "../../module/util/attributeCalculationUtils";
 
 const store = useSkillManagerStore();
-
 const availableSkills = computed(() => store.getActor.skills);
+const proficiencyPerRank = [0, 2, 4, 6, 8];
 
 function getBonus(skill: SkillPF2e): string {
-    const prof = store.getProficiencyAtSelectedLevel(skill.slug);
-    const attribute = skill.attribute;
-    return "+ 12";
+    const proficiency = proficiencyPerRank[store.getProficiencyAtSelectedLevel(skill.slug)] ?? 0;
+    const progression = computeAttributeProgression(store.getActor, skill.attribute, store.selectedLevel);
+    const lastLevelKey = Array.from(progression.keys()).pop() ?? 1;
+
+    const proficiencyBonus = proficiency !== 0 ? proficiency + store.selectedLevel : 0;
+    const attributeBonus = progression.get(lastLevelKey) || 0;
+    const totalBonus = proficiencyBonus + attributeBonus;
+
+    return totalBonus >= 0 ? `+${totalBonus}` : `${totalBonus}`;
 }
+
 </script>
 <template>
     <div class="skill-list">
         <div v-for="skill in availableSkills" :key="skill.slug" class="skill">
             <div class="skill-bonus">{{ getBonus(skill) }}</div>
             <div class="skill-name">{{ skill.label }}</div>
-            <ProficiencyIndicator :skill="skill.slug"></ProficiencyIndicator>
+            <Proficiency :skill="skill.slug"></Proficiency>
         </div>
     </div>
 </template>
@@ -47,6 +55,7 @@ function getBonus(skill: SkillPF2e): string {
 }
 
 .skill-bonus {
-  margin-right: 1.5rem;
+  margin-right: 0.5rem;
+  width: 2rem;
 }
 </style>
