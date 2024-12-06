022464f952a270b8dbb7a1d25ee58dcbfb4cd484
cuyima <vivilotta98@live.de>
2024-12-06 18:32:07 +0100

    add available vs additional explanation tooltip

--- src/templates/components/BottomBar.vue
+++ src/templates/components/BottomBar.vue
@@ -14,8 +14,12 @@ const skillBoosts = store.skillBoosts;
             <button class="submit" type="submit" @click="submit">{{ localize("PF2E.Actor.Character.AttributeBuilder.Complete") }}</button>
         </form>
         <div class="skill-counts container">
-            <div class="count">{{ localize(`${MODULE_ID}.available`) + `: ${skillBoosts.getAvailable(store.selectedLevel)}` }}</div>
-            <div class="count">{{ localize(`${MODULE_ID}.additional`) + `: ${skillBoosts.getAdditional(store.selectedLevel)}` }}</div>
+            <div class="count" :data-tooltip="localize(`${MODULE_ID}.availableTooltip`)">
+                {{ localize(`${MODULE_ID}.available`) + `: ${skillBoosts.getAvailable(store.selectedLevel)}` }}
+            </div>
+            <div class="count" :data-tooltip="localize(`${MODULE_ID}.additionalTooltip`)">
+                {{ localize(`${MODULE_ID}.additional`) + `: ${skillBoosts.getAdditional(store.selectedLevel)}` }}
+            </div>
             <button class="reset" @click="skillBoosts.resetSelection()" :title="localize(`${MODULE_ID}.reset`)">
                 <i class="fa-solid fa-rotate-right"></i>
             </button>
